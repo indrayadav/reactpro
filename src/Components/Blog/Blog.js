6d51ece71e8151d84a12d4aea4257f0eeb3bb5c8
Indra Yadav <ydv.indra@gmail.com>
2019-12-26 16:13:41 +0545

    Data fetched using api

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,41 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import BlogList from './BlogList';
+import './Blog.css'
+
 
 const Blog = (props) => {
+
+    const [posts, setPosts] = useState(
+        []
+      );
+
+      const fetchUsers = async () => {
+        const response = await axios.get(
+          `https://newsapi.org/v2/everything?q=bitcoin&from=2019-11-26&sortBy=publishedAt&apiKey=ad71aa9ff3e44845a7fe5e6249d6c3a5`
+        );
     
-    const bloglistarray = [
-        {
-            id: 1,
-            name: 'Indra',
-            address: 'Kathmandu'
-        },
-        {
-            id: 2,
-            name: 'Kumar',
-            address: 'Janakpur'
-        },
-        {
-            id: 3,
-            name: 'Yadav',
-            address: 'Biratnagar'
-        },
-        {
-            id: 4,
-            name: 'Indra',
-            address: 'Pokhara'
-        }
+        setPosts(response.data.articles);
+      };
+    
+      useEffect(() => {
+        fetchUsers(posts);
+      }, [posts]);
+    
 
-    ]
-
-    const  blogcardarray = bloglistarray.map( (blogcard, i) => {
+    const  blogcardarray = posts.map( (blogcard, i) => {
         
-        return <BlogList id={bloglistarray[i].id}  name={bloglistarray[i].name}  address={bloglistarray[i].address} />
+        return <BlogList id={posts[i].id}  name={posts[i].title}  address={posts[i].description}  img={posts[i].urlToImage} />
     } )
 
     return (
-        <div>
-            <h1> Welcome to Our Post </h1>
-            {blogcardarray}
+            <div className="mw9 center ph3-ns">
+                <div className="cf ph2-ns">
+                 <h1> Welcome to Our Post </h1>
+                     {blogcardarray}
+            </div>
         </div>
     )
 }
--- src/Components/Blog/BlogList.js
+++ src/Components/Blog/BlogList.js
@@ -4,10 +4,13 @@ import 'tachyons';
 const BlogList = (props) => {
 
     return (
-        <div className="blogcard fl w-25 bg-yellow grow ma4 pa5">
-            <img src ={`https://joeschmoe.io/api/v1/${props.name}`} />
-            <h2>{ props.name}</h2>
-            <p>{ props.address}</p>
+        <div className="blogcard grow fl w-100 w-third-ns pa3">
+            <div className="outline bg-white pv4">
+                <img src ={`${props.img}`} />
+                <div class="pa2 pa3-ns"><h2 class="f4 fw5 mv0">{ props.name}</h2>
+                <p class="f6 lh-copy measure mt2 mb0 mid-gray">{ props.address}</p>
+                </div>
+            </div>
         </div>
     )
 }
